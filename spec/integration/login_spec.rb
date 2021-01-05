72688730dd21aab38520973822abb85ea1ed2fd7
Brian Sammit Cruz Rodriguez <briansammit.cruz@gmail.com>
2021-01-05 12:01:46 -0500

    add integration test for log in

--- spec/integration/login_spec.rb
+++ spec/integration/login_spec.rb
@@ -10,4 +10,19 @@ describe 'User', type: :feature do
         click_on 'Sign up'
         expect(page).to have_content('Recent posts')
     end
+    
+    it 'fails to create new user when param missing' do
+        visit '/users/sign_up'
+        fill_in 'user_name', with: 'brian'
+        click_on 'Sign up'
+        expect(page).to have_content('Email')
+    end
+
+    it 'redirects to sign in when not existing user is pass' do
+        visit '/users/sign_in'
+        fill_in 'user_email', with: 'brian@gmail.com'
+        fill_in 'user_password', with: '123456'
+        click_on 'Log in'
+        expect(page).to have_content('Sign in')
+    end
 end
